c39d615442f884b59612715964954b1d72cb1c4b
rusliyana <rusliyana@example.com>
2023-08-03 11:49:27 +0800

    first commit

---
+++ application/controllers/Auth.php
@@ -0,0 +1,83 @@
+<?php
+defined('BASEPATH') OR exit ('No direct script access allowed');
+
+class Auth extends CI_Controller 
+{
+    public function __construct()
+    {
+      parent::__construct();
+      $this->load->library('form_validation');
+    }
+
+    public function index() 
+    {
+       $this->load->view('templates/auth_header'); 
+       $this->load->view('auth/login'); 
+       $this->load->view('templates/auth_footer'); 
+    }
+
+    public function registration()
+    {
+      $this->form_validation->set_rules('name','Name','required|trim');
+      $this->form_validation->set_rules('email','Email','required|trim|valid_email|is_unique[user.email]');
+      $this->form_validation->set_rules('password1','Password','required|trim|min_length[3]|matches[password2]',[
+        'matches'=>'password dont match',
+        'min_lenght' => 'password too short'
+      ]);
+      $this->form_validation->set_rules('password2','Password','required|trim|matches[password1]');
+
+      if($this->form_validation->run() == false) {
+        $data['title'] = 'login registration';
+        $this->load->view('templates/auth_header', $data);
+        $this->load->view('auth/registration');
+        $this->load->view('templates/auth_footer');
+      } else {
+        $data = [
+          'name' => $this->input->post('name'),
+          'email'=> $this->input->post('email'),
+          'password'=> password_hash($this->input->post('password'),PASSWORD_DEFAULT),
+        ];
+
+        $this->db->insert('user',$data);
+        redirect('auth');
+    } 
+  }
+
+  //login
+  public function login()
+  {
+   $email = $this->input->post('email');
+   $password = $this->input->post('password');
+   
+   $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+   $this->form_validation->set_rules('password', 'Password', 'trim|required|md5');
+   
+ $data['title'] = 'Login';
+
+if ($this->form_validation->run() === FALSE) {
+   $this->load->view('templates/auth_header', $data);
+   $this->load->view('auth/login');
+   $this->load->view('templates/auth_footer');
+
+} else {
+   if ($user = $this->user_model->get_user_login($email, $password)) {
+       $this->session->set_userdata('email', $email);
+       $this->session->set_userdata('user_id', $user['id']);
+       $this->session->set_userdata('is_logged_in', true);
+
+       $this->session->set_flashdata('msg_success', 'Login Successful!');
+       redirect('news');
+   } else {
+       $this->session->set_flashdata('msg_error', 'Login credentials does not match!');
+
+       $currentClass = $this->router->fetch_class(); // class = controller
+       $currentAction = $this->router->fetch_method(); // action = function
+
+       redirect("$currentClass/$currentAction");
+       //redirect('user/login');
+   }
+}
+}
+
+  
+}
